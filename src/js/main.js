5667f180958ecb3431564d0f8883d016624d84a6
bxphxmxth <fushs@hotmail.fr>
2021-05-18 19:40:06 +0200

    o_o

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,62 @@
 // ## EXO4
 
 // ### Créez une class [Lieu] avec une propriété nom(string) et personnes(array vide).
+
+class Lieu{
+    constructor(nom,personnes){
+        this.nom = nom;
+        this.personnes = personnes;
+    }
+}
 // ### Puis créez des instances de [Lieu]: MolenGeek, Snack et Maison
+
+let molengeek = new Lieu ("Molengeek", []);
+let snack = new Lieu ("Snack", []);
+let maison = new Lieu ("Maison", []);
+
 // ### Puis créez une class [Personne] avec une propriété nom(string), prenom(string), argent(number) et une methode seDeplacer(), cette methode permet de vous déplacer dans les differents lieux en faisant appel à la méthode embarquer() du bus ou en y allant à pieds.
+class Personne{
+    constructor(nom,prenom,argent){
+        this.nom = nom;
+        this.prenom = prenom;
+        this.argent = argent;
+        this.seDeplacer = (depart, arrivee, transport) =>{
+            if(transport == "pieds"){
+                arrivee.personnes.push(this);
+                depart.personnes.splice(depart.personnes.indexOf(this,1));
+                console.log(`je vais à pieds`);
+            }else if(transport)
+
+        }
+    }
+}
+
 // ### Puis créez une instance de votre personnage.
+
+let naz = new Personne("Aala","Naz", 80);
 // ### Créez une class [Bus] avec une propriéte personnes(array vide), une propriéte caisse(number) et une methode embarquer() (qui sera appelée dans la méthode seDeplacer() de la personne) qui va vérifier si vous possedez l'argent, si oui, il le déduit de votre argent, et le rajoute à sa caisse, et le personnage rentre dans le bus. Le bus cout 2.80.
 // Sinon, il faut y aller à pied.
+class Bus{
+    constructor(personnes, caisse){
+        this.personnes = personnes,
+        this.caisse = caisse;
+        this.embarquer = (passager) =>{
+            if(passager.argent >= 2.80){
+                passager.argent -= 2.80;
+                this.caisse += 2.80;
+                this.personnes.push(passager);
+                this.personnes.splice(this.personnes.indexOf(passager,1));
+                console.log("jss ds lbus");
+                return true;
+            }
+        }
+
+    }
+}
 
 // ### Créez une instance de Bus. 
+
+let bus = new Bus([],0);
 // ### 8h00 Vous êtes à la maison.
 // ### 8h30 Vous prennez le bus vers MolenGeek.
 // ### 8h45 Vous êtes à MolenGeek.
@@ -17,4 +66,4 @@
 
 // ### Faites un console.log() de votre argent, ainsi que l'argent du Bus(modifié)
 
-// 7
+
